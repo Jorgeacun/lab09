0d685b830ac10cdae910b2bf8d01747929b61339
jorge acuna <jacunah13@unc.edu.pe>
2020-11-07 11:41:48 -0500

    PRIMERA VERSION DEL BACKEND

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const express = require('express');
-const { dbconection } = require('./databas/config');
-
- require('dotenv').config();
-
+const { dbconection } = require('./database/config');
+require ('dotenv').config();
+const cors = require('cors');
 
 
 
 //console.log('Hola mundo');
 
-//USUARIO:userJorge
-//password: PIxwc36vEjDuJazx
-//STRING CONEXION:  mongodb+srv://userJorge:<password>@cluster0.ooao6.mongodb.net/test
+// usuario : userJorge
+//password : PIxwc36vEjDuJazx
+// string coneccion: mongodb+srv://userJorge:<password>@cluster0.ooao6.mongodb.net/test
      //CONEXION CON CLAVE CAMBIADA
-//mongodb+srv://userJorge:PIxwc36vEjDuJazx@cluster0.ooao6.mongodb.net/test
+//mongodb+srv://userJorge:PIxwc36vEjDuJazx@cluster0.ooao6.mongodb.net/proyectosDB
+
+
 
 //CODIGO PARA DESPLEGAR EL SERVIDOR
 const server = express();
 
+server.use(cors());
+
+//PRIMERA PETICION GET
 server.get('/', (req, res) => {
     res.json({
         ok: true,
@@ -26,11 +30,10 @@ server.get('/', (req, res) => {
 
 dbconection();
 
-//console.log(process.env);
-
-server.liste(process.env.PORT, () => {
-    console.log('El servidor se está ejecutando en el puerto: ' + 3000),
-        console.log('Edwin Valencia Castillo')
+console.log(process.env);
+server.listen(process.env.PORT, () => {
+    console.log('El servidor se está ejecutando en el puerto: ' + process.env.PORT),
+        console.log('Jorge Luis Acuña Huaripata')
 
 
 
